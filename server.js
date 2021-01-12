2bc3119f36e9dd1b2a8b52ffceaa34d569f8cc08
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-01-12 20:58:18 +0900

    1.3 POC API

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "hiiiii",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
