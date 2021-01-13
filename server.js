00fa71e55a9d9a7c29573fbf717aa1bc58332440
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-01-13 18:47:17 +0900

    1.11 Dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
@@ -5,6 +6,10 @@ const server = new ApolloServer({
   schema,
 });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() =>
+    console.log(`🚀Server is running on http://localhost:${PORT} ✅`)
+  );
