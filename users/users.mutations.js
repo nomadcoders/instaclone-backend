2bb8bded6b0ce3443b2eee411dd0d7160e00edaa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-01-14 18:46:20 +0900

    2.2 Create Account part Three

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import client from "../client";
 
 export default {
@@ -6,7 +7,6 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      // check if username or email are already on DB.
       const existingUser = await client.user.findFirst({
         where: {
           OR: [
@@ -20,8 +20,16 @@ export default {
         },
       });
       console.log(existingUser);
-      // hash password
-      // save and return the user
+      const uglyPassword = await bcrypt.hash(password, 10);
+      return client.user.create({
+        data: {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
+        },
+      });
     },
   },
 };
