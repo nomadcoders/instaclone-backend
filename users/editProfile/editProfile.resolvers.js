5a7d92c8d71b6f5e2908810880fdf6b86b1e6162
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-01-26 16:50:37 +0900

    2.17 File Upload part Four

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,6 +1,9 @@
+import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
+
+console.log(process.cwd());
 
 const resolverFn = async (
   _,
@@ -8,8 +11,9 @@ const resolverFn = async (
   { loggedInUser }
 ) => {
   const { filename, createReadStream } = await avatar;
-  const stream = createReadStream();
-  console.log(stream);
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(process.cwd() + "/uploads/" + filename);
+  readStream.pipe(writeStream);
   let uglyPassword = null;
   if (newPassword) {
     uglyPassword = await bcrypt.hash(newPassword, 10);
