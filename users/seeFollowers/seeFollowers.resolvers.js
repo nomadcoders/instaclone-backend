946598a98c5ea0f97deb96405f98b12dd25e0ec6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-01-27 23:19:49 +0900

    2.24 Followers Pagination part Two

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,15 +3,29 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({
           take: 5,
           skip: (page - 1) * 5,
         });
+      const totalFollowers = await client.user.count({
+        where: { following: { some: { username } } },
+      });
       return {
         ok: true,
         followers,
+        totalPages: Math.ceil(totalFollowers / 5),
       };
     },
   },
