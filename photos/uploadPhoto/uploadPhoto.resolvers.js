52d5ca75aaa74941e939ebb01a44d05c1b4a1366
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-04 16:53:07 +0900

    3.4 Upload Photo part Three

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -5,25 +5,28 @@ export default {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { loggedInUser }) => {
+        let hashtagObj = [];
         if (caption) {
           const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
         }
-        client.photo.create({
+        return client.photo.create({
           data: {
             file,
             caption,
-            hashtags: {
-              connectOrCreate: [
-                {
-                  where: {
-                    hashtag: "#food",
-                  },
-                  create: {
-                    hashtag: "#food",
-                  },
-                },
-              ],
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
             },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
           },
         });
       }
