d8de1a5cb7412cf92d8844dd504016903e646ac8
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-08 16:52:52 +0900

    3.18 protectedResolver Refactor

--- users/users.utils.js
+++ users/users.utils.js
@@ -21,10 +21,15 @@ export const getUser = async (token) => {
 export function protectedResolver(ourResolver) {
   return function (root, args, context, info) {
     if (!context.loggedInUser) {
-      return {
-        ok: false,
-        error: "Please log in to perform this action.",
-      };
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "Please log in to perform this action.",
+        };
+      }
     }
     return ourResolver(root, args, context, info);
   };
