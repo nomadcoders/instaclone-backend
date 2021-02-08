386f5f3149c4424268ccd8a9de13e785a09c692d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-08 19:37:43 +0900

    3.20 S3 Photo Upload part Two

--- shared/shared.utils.js
+++ shared/shared.utils.js
@@ -6,3 +6,18 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET,
   },
 });
+
+export const uploadPhoto = async (file, userId) => {
+  const { filename, createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${userId}-${Date.now()}-${filename}`;
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: "instaclone-uploads",
+      Key: objectName,
+      ACL: "public-read",
+      Body: readStream,
+    })
+    .promise();
+  return Location;
+};
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,6 +2,7 @@ import { createWriteStream } from "fs";
 import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
+import { uploadPhoto } from "../../shared/shared.utils";
 
 const resolverFn = async (
   _,
@@ -10,14 +11,15 @@ const resolverFn = async (
 ) => {
   let avatarUrl = null;
   if (avatar) {
-    const { filename, createReadStream } = await avatar;
+    avatarUrl = await uploadPhoto(avatar, loggedInUser.id);
+    /* const { filename, createReadStream } = await avatar;
     const newFilename = `${loggedInUser.id}-${Date.now()}-${filename}`;
     const readStream = createReadStream();
     const writeStream = createWriteStream(
       process.cwd() + "/uploads/" + newFilename
     );
     readStream.pipe(writeStream);
-    avatarUrl = `http://localhost:4000/static/${newFilename}`;
+    avatarUrl = `http://localhost:4000/static/${newFilename}`; */
   }
   let uglyPassword = null;
   if (newPassword) {
