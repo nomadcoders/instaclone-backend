cd7b287bffb3f3e8501d2e7bd28bd5b77b6ee6aa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-08 16:00:56 +0900

    3.15 isMine

---
+++ comments/comments.resolvers.js
@@ -0,0 +1,10 @@
+export default {
+  Comment: {
+    isMine: ({ userId }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return userId === loggedInUser.id;
+    },
+  },
+};
--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -15,6 +15,12 @@ export default {
       }),
     likes: ({ id }) => client.like.count({ where: { photoId: id } }),
     comments: ({ id }) => client.comment.count({ where: { photoId: id } }),
+    isMine: ({ userId }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return userId === loggedInUser.id;
+    },
   },
   Hashtag: {
     photos: ({ id }, { page }, { loggedInUser }) => {
