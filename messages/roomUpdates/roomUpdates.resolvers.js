2be00a82216c1ef1f95950f6fae2dd17f6abaa5a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-02-10 20:40:20 +0900

    4.9 Filtering Subscriptions part One

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,17 @@
+import { withFilter } from "apollo-server";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(NEW_MESSAGE),
+        ({ roomUpdates }, { id }) => {
+          return roomUpdates.roomId === id;
+        }
+      ),
     },
   },
 };
--- messages/roomUpdates/roomUpdates.typeDefs.js
+++ messages/roomUpdates/roomUpdates.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Subscription {
-    roomUpdates: Message
+    roomUpdates(id: Int!): Message
   }
 `;
